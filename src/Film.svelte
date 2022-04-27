890a92325086569e8307f2d18a4b4f7f60e85157
Jan Podolanko <jan.podolanko11@gmail.com>
2022-04-27 23:02:10 +0200

    likes (dont really work) and deletion

--- src/Film.svelte
+++ src/Film.svelte
@@ -1,6 +1,7 @@
 <script>
+	import { deleteDoc,doc,increment,updateDoc } from "firebase/firestore";
 	import { getDownloadURL,ref } from "firebase/storage";
-	import { storage } from './firebase.js';
+	import { db,storage } from './firebase.js';
 	export let title;
 	export let directors;
 	export let cast;
@@ -8,6 +9,19 @@
 	export let release;
 	export let close = false;
 	export let id;
+
+	async function updateLikes(id, like){
+  if(like > 0){
+    await updateDoc(doc(db, "films", id),{likes: increment(like)})
+  } else {
+    await updateDoc(doc(db, "films", id),{dislikes: increment(like)})
+  }
+}
+
+	async function deleteFilm(id){
+		await deleteDoc(doc(db, "films", id))
+		console.log(id)
+	}
 
 	$: getDownloadURL(ref(storage, `images/${id}.jpeg`)).then((url) =>{
 	let img = document.getElementById("image")
@@ -21,6 +35,9 @@
 <p>Release date: {release.toDate().getDate()}.{release.toDate().getMonth()}.{release.toDate().getFullYear()}</p>
 <p>Directors: {directors}</p>
 <p>Cast: {cast}</p>
+<button id='deleteButton' on:click={()=>deleteFilm(id)}>Delete!</button>
+<!-- <button on:click|once={()=>updateLikes(id,1)}>Like</button>
+<button on:click|once={()=>updateLikes(id,-1)}>Dislike</button> -->
 <p>Synopsis: {synopsis}</p>
 </div>
 
